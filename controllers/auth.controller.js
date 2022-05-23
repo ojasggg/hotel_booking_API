73727ff1e5256647f04e5d885930827e5730dd69
Ojash Gurung <OJASGGG@GMAIL.COM>
2022-05-23 18:15:27 +0545

    Hashed Password using bcryptjs library

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,22 +1,27 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 export const register = async (req, res, next) => {
   try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
       age: req.body.age,
-      password: req.body.password,
+      password: hash,
     });
     await newUser.save();
-    res
-      .status(200)
-      .json({
-        success: true,
-        message: "User has been created successfully",
-        newUser,
-      });
+    res.status(200).json({
+      success: true,
+      message: "User has been created successfully",
+      newUser,
+    });
   } catch (error) {
     next(error);
   }
+};
+
+export const login = async (req, res, next) => {
+  res.send("This is Login page");
 };
