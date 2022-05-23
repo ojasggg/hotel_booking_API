cbb372fa10edec895902f64e05c4a9c4bb681e4b
Ojash Gurung <OJASGGG@GMAIL.COM>
2022-05-23 17:46:39 +0545

    Controller Function without encryption in password is created

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,22 @@
+import User from "../models/user.model.js";
+
 export const register = async (req, res, next) => {
-  res.send("This is Register Controller");
+  try {
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      age: req.body.age,
+      password: req.body.password,
+    });
+    await newUser.save();
+    res
+      .status(200)
+      .json({
+        success: true,
+        message: "User has been created successfully",
+        newUser,
+      });
+  } catch (error) {
+    next(error);
+  }
 };
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -3,6 +3,6 @@ import { register } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/register", register);
+router.post("/register", register);
 
 export default router;
