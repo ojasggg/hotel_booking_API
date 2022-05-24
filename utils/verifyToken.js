b4e1945cc68f66b8361e0aafb55ecb5f86591e5d
Ojash Gurung <OJASGGG@GMAIL.COM>
2022-05-25 00:16:15 +0545

    Created Verify token to check if the user is logged in or not and if logged in then is the user really who he is

---
+++ utils/verifyToken.js
@@ -0,0 +1,12 @@
+import jsonwebtoken from "jsonwebtoken";
+import { createError } from "./createError.js";
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+  if (!token) return next(createError(401, "You are not authenticated!"));
+  jsonwebtoken.verify(token, process.env.SECRET_KEY, (err, user) => {
+    if (err) return next(createError(403, "Token is not valid!"));
+    req.user = user;
+    next();
+  });
+};
