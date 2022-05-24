47ec12f3b790754c0ce01e527065fc757cdd267c
Ojash Gurung <OJASGGG@GMAIL.COM>
2022-05-24 23:46:23 +0545

    Login controller route created

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import { createError } from "../utils/createError.js";
+import jsonwebtoken from "jsonwebtoken";
 
 export const register = async (req, res, next) => {
   try {
@@ -23,5 +25,29 @@ export const register = async (req, res, next) => {
 };
 
 export const login = async (req, res, next) => {
-  res.send("This is Login page");
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return next(createError(404, "User Not Found"));
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!isPasswordCorrect)
+      return next(createError(404, "Invalid Credentials"));
+
+    const token = jsonwebtoken.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.SECRET_KEY
+    );
+    const { password, isAdmin, ...other } = user._doc;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json({ ...other });
+  } catch (error) {
+    next(error);
+  }
 };
